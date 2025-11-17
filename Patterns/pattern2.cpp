d4b18e89ee6eed28008f359c6a96971f9689c481
Prikshit <callmeprikshit@gmail.com>
2025-11-17 10:14:45 +0530

    Create pattern2.cpp

---
+++ Patterns/pattern2.cpp
@@ -0,0 +1,20 @@
+// * 
+// * * 
+// * * * 
+// * * * * 
+// * * * * *
+// * * * * * *
+
+
+class Solution {
+  public:
+    void printTriangle(int n) {
+        // code here
+        for(int i=0;i<n;i++){
+        for(int j=0;j<=i;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+        }
+    }
+};
